1769d809b38262c9b269dd017e9e94cba68619da
cooperren <cooperren@tencent.com>
2021-12-21 11:30:46 +0800

    add deprecated field

--- contracts/CptData.sol
+++ contracts/CptData.sol
@@ -18,6 +18,7 @@ contract CptData {
         string jsonSchema;
         uint version;
         uint8 cptType;
+        bool deprecated;
 
         //store signature
         Signature signature;
@@ -36,6 +37,8 @@ contract CptData {
 
         uint version;
 
+        bool deprecated;
+
         mapping (uint => CptInfo) verMap;
     }
 
@@ -53,7 +56,7 @@ contract CptData {
 
 
     function createCpt(address identity, string memory did, string memory title, string memory jsonSchema, uint8 cptType,
-        uint8 v, bytes32 r, bytes32 s) public returns (uint) {
+        uint8 v, bytes32 r, bytes32 s) public returns (uint8) {
         
         //根据did获取地址,校验发送地址是否与did绑定地址相同
         require (identity == didController.getDidAddress(did));
@@ -68,6 +71,7 @@ contract CptData {
         Cpt storage cpt = cptMap[cptId];
         cpt.id = cptId;
         cpt.did = did;
+        cpt.deprecated = false;
 
         uint newVersion = cpt.version + 1;
         cpt.version = newVersion;
@@ -76,6 +80,7 @@ contract CptData {
         cptInfo.title = title;
         cptInfo.jsonSchema = jsonSchema;
         cptInfo.cptType = cptType;
+        cptInfo.deprecated = false;
         cptInfo.created = block.timestamp;
         cptInfo.updated = block.timestamp;
         cptInfo.version = newVersion;
@@ -85,18 +90,40 @@ contract CptData {
     }
 
     
-    function getCpt(uint id, uint version) public view returns (string memory, string memory, string memory, uint8, uint, uint256, uint256, uint8, bytes32, bytes32) {
+    function getCptInfo(uint id) public view returns (uint, string memory, uint, bool) {
 
         Cpt storage cpt = cptMap[id];
+
+        return (cpt.id, cpt.did, cpt.version, cpt.deprecated);
+    }
+    
+    function getCptInfo(uint id, uint version) public view returns (string memory, string memory, 
+        string memory, uint8, bool, uint, uint256, uint256, uint8, bytes32, bytes32) {
+
+        Cpt storage cpt = cptMap[id];
+
+        require (cpt.deprecated == false);
+
+        if(version == 0) version = cpt.version;
         CptInfo storage cptInfo = cpt.verMap[version];
 
-        return (cpt.did, cptInfo.title,cptInfo.jsonSchema, cptInfo.cptType, cptInfo.version, cptInfo.created,cptInfo.updated, 
+        return (cpt.did, cptInfo.title,cptInfo.jsonSchema, cptInfo.cptType, cptInfo.deprecated, cptInfo.version, cptInfo.created,cptInfo.updated, 
             cptInfo.signature.v, cptInfo.signature.r, cptInfo.signature.s);
     }
 
 
+    function deprecateCpt(uint id, uint version) public returns (uint8) {
+
+        Cpt storage cpt = cptMap[id];
+        CptInfo storage cptInfo = cpt.verMap[version];
+        cptInfo.deprecated = true;
+
+        return 0;
+    }
+
+
     function updateCpt(uint id, address identity, string memory did, string memory title, string memory jsonSchema, uint8 cptType,
-        uint8 v, bytes32 r, bytes32 s) public returns (uint) {
+        uint8 v, bytes32 r, bytes32 s) public returns (uint8) {
         
         //根据did获取地址,校验发送地址是否与did绑定地址相同
         require (identity == didController.getDidAddress(did));
@@ -117,6 +144,7 @@ contract CptData {
         cptInfo.title = title;
         cptInfo.jsonSchema = jsonSchema;
         cptInfo.cptType = cptType;
+        cptInfo.deprecated = false;
         cptInfo.created = block.timestamp;
         cptInfo.updated = block.timestamp;
         cptInfo.version = newVersion;
